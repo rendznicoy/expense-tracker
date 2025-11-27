cbcc5a04963d43426e475d91587031c8a9f9cf70
rendznicoy <rendzdelosreyes@gmail.com>
2025-11-27 16:01:18 +0800

    [UPDATE] [VERSION 0] - Created initial text for login page.

---
+++ src/pages/Auth/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcum on my Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please send nutes to log in
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
