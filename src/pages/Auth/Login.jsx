4b703d0f9b0314e7b57e12d7e3372ca2c82c54f1
rendznicoy <rendzdelosreyes@gmail.com>
2025-11-28 11:23:34 +0800

    [UPDATE] [VERSION 0] - Created AuthLayout for initial login design.

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
 
 const Login = () => {
   return (
-    <div>
+    <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Welcum on my Back</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please send nutes to log in
         </p>
       </div>
-    </div>
+    </AuthLayout>
   );
 };
 
