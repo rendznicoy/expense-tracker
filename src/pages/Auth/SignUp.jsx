958b7c956d89af618619cac51598b505db89b8a3
rendznicoy <rendzdelosreyes@gmail.com>
2025-11-28 16:56:57 +0800

    [UPDATE] [VERSION 1] - Sign Up page partially finished with a frontend image selector ready for use, needs backend integration.

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -3,6 +3,7 @@ import AuthLayout from "../../components/layouts/AuthLayout";
 import { useNavigate, Link } from "react-router-dom";
 import Input from "../../components/inputs/input";
 import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -15,7 +16,30 @@ const SignUp = () => {
   const navigate = useNavigate();
 
   // Handle Sign Up Form Submission
-  const handleSignUp = async (e) => {};
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please enter your name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    // Sign Up API Call
+  };
 
   return (
     <AuthLayout>
@@ -54,6 +78,18 @@ const SignUp = () => {
               />
             </div>
           </div>
+          {error && <p className="text-red-500 text-xs pb-2.5"> {error} </p>}
+
+          <button type="submit" className="btn-primary">
+            SIGN UP
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
